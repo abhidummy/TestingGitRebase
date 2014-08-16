63dd87c64270484550034a2dcb3ea78693180e2b
abhidummy <abhijit_146@yahoo.co.in>
2014-08-16 12:29:31 +0530

    adding Divide functionality

--- calcBL/SimpleFunctions.cs
+++ calcBL/SimpleFunctions.cs
@@ -19,5 +19,9 @@ namespace MyNamespace.CalcBL
         {
             return num1 * num2;
         }
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
     }
 }
